a8d428cb297fb4b98f745f88c9fd91250168cfa7
Eric Oliveira <oiericoi@hotmail.com>
2024-08-17 17:59:09 -0300

    build: add base springboot project

---
+++ src/main/java/com/cidadao/conectado/CidadaoConectadoApplication.java
@@ -0,0 +1,13 @@
+package com.cidadao.conectado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadaoConectadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadaoConectadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidadao/conectado/CidadaoConectadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadao.conectado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaoConectadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
