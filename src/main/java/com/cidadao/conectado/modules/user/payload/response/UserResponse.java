b795f18f576bb81db0b45395abd1cbdab1146f2a
Eric Oliveira <oiericoi@hotmail.com>
2024-08-20 20:27:55 -0300

    feat: add UserResponse payload

---
+++ src/main/java/com/cidadao/conectado/modules/user/payload/response/UserResponse.java
@@ -0,0 +1,51 @@
+package com.cidadao.conectado.modules.user.payload.response;
+
+import java.sql.Timestamp;
+
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Timestamp registerDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Timestamp registerDate) {
+        this.registerDate = registerDate;
+    }
+}
