20db0145278908b107f6aef2e9c85f48e21a0daf
Eric Nascimento <oiericoi@hotmail.com>
2024-08-25 20:34:20 -0300

    feat: add update user request

---
+++ src/main/java/com/cidadao/conectado/modules/user/payload/request/UpdateUserRequest.java
@@ -0,0 +1,13 @@
+package com.cidadao.conectado.modules.user.payload.request;
+
+public class UpdateUserRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
