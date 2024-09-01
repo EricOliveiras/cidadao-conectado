7c27c2999418a93a005d3c7b2f4ba0eac1b377da
Eric Nascimento <oiericoi@hotmail.com>
2024-09-01 15:01:11 -0300

    refactor: add validations

--- src/main/java/com/cidadao/conectado/modules/user/payload/request/UpdateUserRequest.java
+++ src/main/java/com/cidadao/conectado/modules/user/payload/request/UpdateUserRequest.java
@@ -1,6 +1,14 @@
 package com.cidadao.conectado.modules.user.payload.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class UpdateUserRequest {
+    @Valid
+
+    @NotBlank(message = "O nome não pode estar vazio")
+    @NotNull
     private String name;
 
     public String getName() {
