759937b75b25d384f0a38c75bbd0f2ff034e2602
Eric Nascimento <oiericoi@hotmail.com>
2024-09-08 22:49:54 -0300

    feat: add proposal repository

---
+++ src/main/java/com/cidadao/conectado/modules/proposal/ProposalRepository.java
@@ -0,0 +1,6 @@
+package com.cidadao.conectado.modules.proposal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+}
