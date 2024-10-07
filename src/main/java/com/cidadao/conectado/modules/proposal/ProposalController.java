d33cd7bd927abaa65dec126182adcef3df714470
Eric Nascimento <oiericoi@hotmail.com>
2024-10-07 18:42:50 -0300

    refactor: modify proposal controller

--- src/main/java/com/cidadao/conectado/modules/proposal/ProposalController.java
+++ src/main/java/com/cidadao/conectado/modules/proposal/ProposalController.java
@@ -28,4 +28,20 @@ public class ProposalController {
     public ResponseEntity<List<ProposalResponse>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(iProposalService.readAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProposalResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iProposalService.read(id));
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<ProposalResponse> update(@Valid @PathVariable Long id, @RequestBody CreateProposalRequest proposal) {
+        ProposalResponse proposalResponse = iProposalService.update(id, proposal);
+        return ResponseEntity.status(HttpStatus.OK).body(proposalResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iProposalService.delete(id));
+    }
 }
